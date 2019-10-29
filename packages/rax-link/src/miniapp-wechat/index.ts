c52310ddf81eb9478bd9e91e23a16c1bef2e3f8d
fushen <fushen.jzw@alibaba-inc.com>
2019-10-29 10:45:21 +0800

    feat: support wehcat miniprogram link

--- packages/rax-link/src/miniapp-wechat/index.ts
+++ packages/rax-link/src/miniapp-wechat/index.ts
@@ -1,5 +1,6 @@
 import fmtEvent from './fmtEvent';
 
+declare const wx: any;
 const noop = () => {};
 /**
  * miniappHref value：navigate:abc?a=1 redirect:abc?a=1 switchTab:a/b/c navigateBack:2
@@ -8,7 +9,7 @@ const noop = () => {};
 
 Component({
   data: {},
-  props: {
+  properties: {
     className: '',
     style: '',
     mpHref: '',
@@ -16,14 +17,17 @@ Component({
     onPress: noop,
     onClick: noop,
   },
+  options: {
+    styleIsolation: 'apply-shared',
+  },
   methods: {
     onTap(e) {
       const event = fmtEvent(this.props, e);
       // onPress for  backward compatibility
       if (this.props.onClick === noop) {
-        this.props.onPress(event);
+        this.triggerEvent('onPress', event);
       } else {
-        this.props.onClick(event);
+        this.triggerEvent('onClick', event);
       }
       const miniappHref = this.props.miniappHref || this.props.mpHref;
       if (miniappHref) {
@@ -32,19 +36,19 @@ Component({
         const target = splits[1] || actionName;
         switch (actionName) {
           case 'navigate':
-            my.navigateTo({ url: target });
+            wx.navigateTo({ url: target });
             break;
           case 'redirect':
-            my.redirectTo({ url: target });
+            wx.redirectTo({ url: target });
             break;
           case 'switchTab':
-            my.switchTab({ url: target });
+            wx.switchTab({ url: target });
             break;
           case 'navigateBack':
-            my.navigateBack({ delta: target });
+            wx.navigateBack({ delta: target });
             break;
           default:
-            my.navigateTo({ url: target });
+            wx.navigateTo({ url: target });
         }
       }
     },

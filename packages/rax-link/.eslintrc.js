afaa1459ed7bee8f5224a6bc4ba86fd8a6401bac
fushen <fushen.jzw@alibaba-inc.com>
2019-10-29 21:20:44 +0800

    chore: lint

---
+++ packages/rax-link/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['eslint-config-rax/typescript'],
+  globals: {
+    Component: true,
+    Page: true,
+    App: true,
+    my: true,
+    wx: true,
+  },
+  rules: {
+    'no-return-assign': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'new-cap': [
+      'error',
+      {
+        newIsCap: true,
+        capIsNewExceptions: ['Component', 'Page', 'App'],
+      },
+    ],
+  },
+};

43b5d81bdd6757cc2c8d5447d889108eb52c2d4a
水澜 <shuilan.cj@taobao.com>
2019-12-26 13:31:57 +0800

    fix: disable webp in node

--- packages/rax-picture/src/webp.ts
+++ packages/rax-picture/src/webp.ts
@@ -36,7 +36,10 @@ function setLocalStorage(isSupport: boolean, type: string) {
 
 export function isSupport(callback: (status: boolean) => void, type = 'lossy') {
   if ('function' === typeof callback) {
-    if (isWeex || isNode || window.navigator.userAgent.match(/PHA/)) {
+    // When render in server side, it is hard to judge if client supports support webp
+    if (isNode) {
+      callback(false);
+    } else if (isWeex || window.navigator.userAgent.match(/PHA/)) {
       callback(true);
     } else if (window.navigator.userAgent.match(/windows|win32/i) || isIOS && window.navigator.userAgent.match(/UCBrowser/i)) {
       callback(false);

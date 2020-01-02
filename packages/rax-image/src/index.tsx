90ce6ea4a2b887c214923a326b6b29046d90360a
yinhf <yinhangfeng@gmail.com>
2020-01-02 17:34:42 +0800

    fix: 修复 rax-image source 改变不会更新图片的问题

--- packages/rax-image/src/index.tsx
+++ packages/rax-image/src/index.tsx
@@ -1,85 +1,94 @@
-import {
-  createElement,
-  useState,
-  useCallback
-} from 'rax';
+import { createElement, useState, useCallback } from 'rax';
 import { isWeex } from 'universal-env';
 import { ImageProps, Source, ImageLoadEvent, ImageNativeProps } from './types';
 
-function Image(props: ImageProps) {
-  const [source, setSource] = useState<Source>(props.source);
+const EMPTY_SOURCE = {} as any as Source;
 
-  const onError = useCallback((e: ImageLoadEvent) => {
-    const { fallbackSource, onError = () => {} } = props;
-    if (
-      fallbackSource &&
-        fallbackSource.uri &&
-        source.uri !== fallbackSource.uri
-    ) {
-      setSource(fallbackSource);
-    }
-    onError(e);
-  }, []);
+interface ErrorState {
+  uri?: string;
+}
 
-  const onLoad = useCallback((e: ImageLoadEvent) => {
-    const { onLoad = () => {} } = props;
-    if (typeof e.success !== 'undefined') {
-      if (e.success) {
-        onLoad(e);
-      } else {
-        onError(e);
+function Image({
+  source,
+  fallbackSource,
+  onLoad,
+  onError,
+  style,
+  resizeMode,
+  ...otherProps
+}: ImageProps) {
+  source = source || EMPTY_SOURCE;
+  fallbackSource = fallbackSource || EMPTY_SOURCE;
+  const nativeProps: ImageNativeProps = otherProps as any;
+  const [errorState, setErrorState] = useState<ErrorState>({});
+
+  nativeProps.onError = useCallback(
+    (e: ImageLoadEvent) => {
+      if (errorState.uri === undefined) {
+        setErrorState({
+          uri: source.uri,
+        });
       }
-    } else if (typeof e.currentTarget !== 'undefined') {
-      if (
-        e.currentTarget.naturalWidth > 1 &&
+      onError && onError(e);
+    },
+    [source.uri, onError, errorState]
+  );
+
+  nativeProps.onLoad = useCallback(
+    (e: ImageLoadEvent) => {
+      if (e.success !== null) {
+        if (e.success) {
+          onLoad && onLoad(e);
+        } else {
+          onError && onError(e);
+        }
+      } else if (e.currentTarget != null) {
+        if (
+          e.currentTarget.naturalWidth > 1 &&
           e.currentTarget.naturalHeight > 1
-      ) {
-        onLoad(e);
-      } else {
-        onError(e);
+        ) {
+          onLoad && onLoad(e);
+        } else {
+          onError && onError(e);
+        }
       }
+    },
+    [onLoad, onError]
+  );
+
+  if (errorState.uri !== undefined) {
+    if (errorState.uri !== source.uri) {
+      errorState.uri = undefined;
+    } else if (fallbackSource.uri != null) {
+      source = fallbackSource;
     }
-  }, []);
-
-  // Source must a object
-  if (source && source.uri) {
-    const { style = {} } = props;
-    const { width, height, uri } = source;
-
-    const nativeProps: ImageNativeProps = {
-      ...props,
-      src: uri,
-      onLoad,
-      onError
-    };
-
-    nativeProps.style = {
-      width,
-      height,
-      ...style
-    };
-
-    delete nativeProps.source;
-
-    // for cover and contain
-    const resizeMode = nativeProps.resizeMode || nativeProps.style.resizeMode;
-    if (resizeMode) {
-      if (isWeex) {
-        nativeProps.resize = resizeMode;
-        nativeProps.style.resizeMode = resizeMode;
-      } else {
-        nativeProps.style.objectFit = resizeMode;
-      }
-    }
-
-    // Set default quality to "original" in weex avoid image be optimized unexpect
-    return isWeex ? (
-      <image quality="original" {...nativeProps} />
-    ) : (
-      <img {...nativeProps} />
-    );
   }
-  return null;
+
+  const { width, height, uri } = source;
+  nativeProps.src = uri;
+  nativeProps.style = {
+    width,
+    height,
+    ...style,
+  };
+
+  // for cover and contain
+  resizeMode = resizeMode || nativeProps.style.resizeMode;
+  if (resizeMode) {
+    if (isWeex) {
+      nativeProps.resize = resizeMode;
+      nativeProps.style.resizeMode = resizeMode;
+    } else {
+      nativeProps.style.objectFit = resizeMode as any;
+    }
+  }
+
+  // Set default quality to "original" in weex avoid image be optimized unexpect
+  return isWeex ? (
+    <image quality="original" {...nativeProps} />
+  ) : (
+    <img {...nativeProps} />
+  );
 }
 
 export default Image;

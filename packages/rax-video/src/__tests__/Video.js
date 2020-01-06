edc4648134bfe637fda3ef39edf16822fe47a50c
yacheng.sz <yacheng.sz@taobao.com>
2020-01-06 15:44:24 +0800

    feat: add test for video

--- packages/rax-video/src/__tests__/Video.js
+++ packages/rax-video/src/__tests__/Video.js
@@ -1,25 +1,36 @@
 global.callNative = null;
 import {createElement} from 'rax';
 import renderer from 'rax-test-renderer';
-import Video from '../';
+import Video from '../../lib/index';
 
 describe('Video', () => {
   it('render tag Video', () => {
     const component = renderer.create(
-      <Video />
+      <Video style={{ width: 750, height: 400 }}
+        src="https://cloud.video.taobao.com/play/u/2780279213/p/1/e/6/t/1/d/ld/36255062.mp4" />
     );
     let tree = component.toJSON();
     expect(tree.tagName).toEqual('VIDEO');
-    expect(tree.children[0].tagName).toEqual('SOURCE');
   });
 
-  it('props on Video', () => {
+  it('use default props on Video', () => {
     const mockPress = jest.fn();
     const component = renderer.create(
-      <Video />
+      <Video style={{ width: 750, height: 400 }}
+        src="https://cloud.video.taobao.com/play/u/2780279213/p/1/e/6/t/1/d/ld/36255062.mp4" />
     );
     let tree = component.toJSON();
     expect(tree.attributes.controls).toBe(true);
     expect(tree.attributes['webkit-playsinline']).toBe(true);
+  });
+
+  it('use auto props on Video', () => {
+    const mockPress = jest.fn();
+    const component = renderer.create(
+      <Video autoPlay={true} style={{ width: 750, height: 400 }}
+        src="https://cloud.video.taobao.com/play/u/2780279213/p/1/e/6/t/1/d/ld/36255062.mp4" />
+    );
+    let tree = component.toJSON();
+    expect(tree.attributes.autoPlay).toBe(true);
   });
 });
--- packages/rax-video/src/__tests__/Video.weex.js
+++ packages/rax-video/src/__tests__/Video.weex.js
@@ -1,6 +1,6 @@
 import {createElement} from 'rax';
 import renderer from 'rax-test-renderer';
-import Video from '../';
+import Video from '../../lib/index';
 
 jest.mock('universal-env', () => {
   return {
@@ -11,7 +11,8 @@ jest.mock('universal-env', () => {
 describe('Video in weex', () => {
   it('render tag Video', () => {
     const component = renderer.create(
-      <Video />
+      <Video style={{ width: 750, height: 400 }}
+        src="https://cloud.video.taobao.com/play/u/2780279213/p/1/e/6/t/1/d/ld/36255062.mp4" />
     );
     let tree = component.toJSON();
 

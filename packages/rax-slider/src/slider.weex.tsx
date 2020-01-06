36629a59a95a21fffd01bcc4af9e769052ae5fd1
yangfan <18767120422@163.com>
2020-01-06 16:29:31 +0800

    fix: slideTo

--- packages/rax-slider/src/slider.weex.tsx
+++ packages/rax-slider/src/slider.weex.tsx
@@ -1,22 +1,47 @@
-import { createElement, ForwardRefExoticComponent, forwardRef } from 'rax';
+import { createElement, ForwardRefExoticComponent, forwardRef, useImperativeHandle, useState } from 'rax';
 import { SliderProps } from './types';
 import './index.css';
 
 const Slider: ForwardRefExoticComponent<SliderProps> = forwardRef(
   (props, ref) => {
+    // This value determines the index of current shown slide in Weex. The default value is 0.
+    const [index, setIndex] = useState(props.index || 0);
     const {
       autoPlay,
       showsPagination,
       paginationStyle,
       autoPlayInterval,
-      index,
       loop,
       width,
       height,
       style,
       children,
+      onChange,
       ...rest
     } = props;
+
+    // @ts-ignore
+    // Ignore 'HTMLElement error', because it's a Weex Component.
+    useImperativeHandle(ref, () => ({
+      slideTo: (newIndex: number) => {
+        setIndex(newIndex);
+      }
+    }));
+
+    const handleChange = (result: any) => {
+      let currentIndex = 0;
+      // https://weex.apache.org/docs/components/slider.html#rax-example
+      // The past Weex docs shows that result is an number, But in the new Weex App returns {index:x, timestamp:...}
+      if (typeof result === "number") {
+        currentIndex = result;
+      }
+      if (typeof result === "object" && typeof result.index !== "undefined") {
+        currentIndex = result.index;
+      }
+      setIndex(currentIndex);
+      onChange && onChange(result);
+    }
+
     return (
       <slider
         {...rest}
@@ -26,6 +51,7 @@ const Slider: ForwardRefExoticComponent<SliderProps> = forwardRef(
         interval={autoPlayInterval}
         infinite={loop}
         index={index}
+        onChange={handleChange}
         style={{ width, height, ...style }}
       >
         {showsPagination && (

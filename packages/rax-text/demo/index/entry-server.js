e150e240afeadabaefc84cee6ae32921600b7089
水澜 <shuilan.cj@taobao.com>
2020-01-21 15:48:53 +0800

    refactor: js to jsx

--- packages/rax-text/demo/index/entry-server.js
+++
@@ -1,7 +0,0 @@
-import { createElement } from 'rax';
-import { renderToString } from 'rax-server-renderer';
-import App from './index';
-
-export default function() {
-  return renderToString(<App />);
-};

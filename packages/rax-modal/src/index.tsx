f450c472ac03debccce4d0e18285322970ef4ff3
fushen <fushen.jzw@alibaba-inc.com>
2020-03-23 19:33:14 +0800

    chore: add comments

--- packages/rax-modal/src/index.tsx
+++ packages/rax-modal/src/index.tsx
@@ -46,6 +46,7 @@ function Modal(props: ModalProps) {
   if (typeof duration === 'number') {
     duration = [duration, duration];
   } else if (duration.length === 1) {
+    // If duration's length is one, in and out are the same.
     duration = [duration[0], duration[0]];
   }
 

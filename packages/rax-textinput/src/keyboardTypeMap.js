fb94af486dd429362eccd8a560b16a8a4c7cc16d
qianxin.yy <qianxin.yy@alibaba-inc.com>
2020-04-09 17:33:17 +0800

    feat: compate secureTextEntry

--- packages/rax-textinput/src/keyboardTypeMap.js
+++ packages/rax-textinput/src/keyboardTypeMap.js
@@ -1,15 +1,17 @@
+import { isQuickApp } from 'universal-env';
+
 const keyboardTypeMap = {
   default: 'text',
   'ascii-capable': 'text',
   'numbers-and-punctuation': 'number',
-  url: 'url',
+  url: isQuickApp ? 'text' : 'url',
   'number-pad': 'number',
   'phone-pad': 'tel',
   'name-phone-pad': 'text',
   'email-address': 'email',
   'decimal-pad': 'number',
   twitter: 'text',
-  'web-search': 'search',
+  'web-search': isQuickApp ? 'text' : 'search',
   numeric: 'number'
 };
 

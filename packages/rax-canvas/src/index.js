1e56ba5c2cb87b1b382cbee1fd0a082400e84510
ChrisCindy <yxy0919@foxmail.com>
2020-04-10 18:17:56 +0800

    fix(rax-canvas): make width/height consistent with style

--- packages/rax-canvas/src/index.js
+++ packages/rax-canvas/src/index.js
@@ -1,8 +1,27 @@
 import { createElement, Component, createRef } from 'rax';
 import { isWeex } from 'universal-env';
 import { enable, WeexBridge, Image as GImage } from '@gcanvas/core';
+import { rpx2px } from 'universal-unit-tool';
 
 import findDOMNode from 'rax-find-dom-node';
+
+/**
+ * process user input width/height in style
+ * @param userInputDimension
+ */
+function processCanvasDimension(userInputDimension) {
+  if (typeof userInputDimension === 'number') {
+    return userInputDimension;
+  }
+  if (typeof userInputDimension === 'string') {
+    const numberValue = parseInt(userInputDimension);
+    if (userInputDimension.endsWith('rpx')) {
+     return rpx2px(numberValue);
+    }
+    return numberValue;
+  }
+  return 300; // Default value
+}
 
 class Canvas extends Component {
   constructor(props) {
@@ -22,8 +41,8 @@ class Canvas extends Component {
   render() {
     const { style = {}, width, height } = this.props;
     const customStyle = {
-      width: width !== undefined ? width : style.width,
-      height: height !== undefined ? height : style.height,
+      width: width !== undefined ? width : processCanvasDimension(style.width),
+      height: height !== undefined ? height : processCanvasDimension(style.height),
     };
 
     if (isWeex) {
@@ -32,7 +51,7 @@ class Canvas extends Component {
         ...customStyle
       }} ref={this.canvas} />;
     } else {
-      return <canvas {...this.props} width={parseInt(customStyle.width)} height={parseInt(customStyle.height)} ref={this.canvas} />;
+      return <canvas {...this.props} width={customStyle.width} height={customStyle.height} ref={this.canvas} />;
     }
   }
 }

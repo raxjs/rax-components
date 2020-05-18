e4f5e1969a5b7d007262d05ce84800d4a360ac66
寻御 <lingjun.mlj@alibaba-inc.com>
2020-05-18 16:02:52 +0800

    feat(video): support methods & props

--- packages/rax-video/src/miniapp/index.ts
+++ packages/rax-video/src/miniapp/index.ts
@@ -1,6 +1,39 @@
 import fmtEvent from './fmtEvent';
 
 const noop = () => {};
+
+/**
+ * supportMethods
+ *
+ * @see https://opendocs.alipay.com/mini/component/video
+ */
+const supportMethods = [
+  'onPlay',
+  'onPause',
+  'onEnded',
+  'onTimeUpdate',
+  'onLoading',
+  'onStop',
+  'onRenderStart',
+  'onError',
+  'onFullScreenChange',
+  'onClick',
+  'onUserAction',
+];
+
+const cmptProps = supportMethods.reduce((prev, current) => {
+  prev[current] = noop;
+  return prev;
+}, {});
+
+const cmptMethods = supportMethods.reduce((prev, current) => {
+  prev[current] = function(e) {
+    const event = fmtEvent(this.props, e);
+    this.props[current](event);
+  };
+  return prev;
+}, {});
+
 Component({
   data: {},
   props: {
@@ -10,20 +43,13 @@ Component({
     loop: false,
     style: '',
     className: '',
-    onClick: noop,
-    onEnded: noop,
     muted: false,
     id: '',
-    extraInfo: {}
+    extraInfo: {},
+    mobilenetHintType: 1,
+    ...cmptProps,
   },
   methods: {
-    onClick(e) {
-      const event = fmtEvent(this.props, e);
-      this.props.onClick(event);
-    },
-    onEnded(e) {
-      const event = fmtEvent(this.props, e);
-      this.props.onEnded(event);
-    }
+    ...cmptMethods,
   },
 });

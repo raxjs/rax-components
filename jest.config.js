b5910b4a2832cfc6f11737deaf9b8844ba71452e
水澜 <shuilan.cj@taobao.com>
2020-06-23 12:19:43 +0800

    chore: clear setup tests for error handler

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,6 @@ module.exports = {
     'jest-localstorage-mock'
   ],
   'setupFilesAfterEnv': [
-    './scripts/jest/setupTests.js',
     './scripts/jest/setupEnzyme.js',
   ],
   'moduleNameMapper': {
@@ -24,7 +23,6 @@ module.exports = {
     '/__files__/',
     '/lib/',
     '/dist/',
-    '/es/',
   ]
 };
 

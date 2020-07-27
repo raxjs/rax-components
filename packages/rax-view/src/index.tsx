b5b39aea002b3798412dcc45a90f3bf96aa3e25d
fushen <fushen.jzw@alibaba-inc.com>
2020-07-27 14:19:43 +0800

    feat: support base props

--- packages/rax-view/src/index.tsx
+++ packages/rax-view/src/index.tsx
@@ -1,6 +1,6 @@
 import { createElement, forwardRef, ForwardRefExoticComponent, RefAttributes, HTMLAttributes } from 'rax';
 import cx from 'classnames/dedupe';
-import { isWeex } from 'universal-env';
+import { isWeex, isMiniApp } from 'universal-env';
 import './index.css';
 
 export type ViewProps = RefAttributes<HTMLDivElement> & HTMLAttributes<HTMLDivElement>;
@@ -8,6 +8,13 @@ export type ViewProps = RefAttributes<HTMLDivElement> & HTMLAttributes<HTMLDivEl
 const View: ForwardRefExoticComponent<ViewProps> = forwardRef(
   (props, ref) => {
     const { className, style, ...rest } = props;
+    if (isMiniApp) {
+      // For miniapp runtime pre-compile
+      const { onAppear, onDisappear, onFirstAppear } = props;
+      return <view
+       {...rest} onAppear={onAppear} onDisappear={onDisappear} onFirstAppear={onFirstAppear}
+       ref={ref} className={`rax-view ${className}`} style={style} />;
+    }
     return <div {...rest} ref={ref} className={cx( isWeex ? '' : 'rax-view', className)} style={style} />;
   }
 );

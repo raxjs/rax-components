1c20ca5e131afb435c20f321c1f94213554d33ae
逆葵 <xianyong.yxy@alibaba-inc.com>
2020-09-30 11:17:18 +0800

    refactor(countdown): rename css

--- packages/rax-countdown/src/index.js
+++ packages/rax-countdown/src/index.js
@@ -20,10 +20,10 @@ function Time(props) {
 
   const numList = displayNum.split('');
   const numListLength = numList.length - 1;
-  return <View className="item" style={{...styles.item, ...timeWrapStyle}}>
+  return <View className="rax-countdown-item" style={{...timeWrapStyle}}>
     {
       timeBackground ?
-        <Image className="background" source={timeBackground} style={{...styles.background, ...timeBackgroundStyle}} /> :
+        <Image className="rax-countdown-background" source={timeBackground} style={{...timeBackgroundStyle}} /> :
         null
     }
     {
@@ -164,7 +164,7 @@ class Index extends Component {
     }
     let lastPlaintextIndex = 0;
 
-    return <View className="main" style={styles.main}>
+    return <View className="rax-countdown-main">
       {
         matchlist.map((val, index) => {
           if (val === -1) {// don't forget the potential plain text after last matched item

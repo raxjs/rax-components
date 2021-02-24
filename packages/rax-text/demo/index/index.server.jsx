3a60b183a03d9b48f92faeb7b6b6143de12ad0de
fushen <fushen.jzw@alibaba-inc.com>
2021-02-24 17:08:39 +0800

    fix: rax text build config

--- packages/rax-text/demo/index/index.server.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { createElement } from 'rax';
-import { renderToString } from 'rax-server-renderer';
-import App from './app';
-
-export default function() {
-  return renderToString(<App />);
-};

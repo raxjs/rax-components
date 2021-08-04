c275c49e77048ab8f2e6667049a476baba9b9998
fushen <fushen.jzw@alibaba-inc.com>
2021-08-04 14:25:40 +0800

    chore: resolve conflict

--- packages/rax-view/src/index.tsx
+++ packages/rax-view/src/index.tsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import { createElement, useRef, forwardRef, ForwardRefExoticComponent, MutableRefObject } from 'rax';
-import cx from 'classnames/dedupe';
-import { isWeex } from 'universal-env';
-import wrapDefaultProperties from './utils/wrapDefaultProperties';
-import { ViewProps } from './types';
-import './index.css';
-
-interface ViewRef extends MutableRefObject<HTMLDivElement> {
-  triggeredAppear: boolean;
-=======
 import { isMiniApp, isWeb, isWeChatMiniProgram } from 'universal-env';
 import ViewCommon from './common';
 import ViewAliMiniApp from './miniapp/ali';
@@ -24,27 +13,6 @@ if (isWeb) {
   View = ViewWechatMiniProgram;
 } else {
   View = ViewCommon;
->>>>>>> master
 }
 
-const View: ForwardRefExoticComponent<ViewProps> = forwardRef(
-  (props, ref) => {
-    const selfRef: ViewRef = useRef(null) as ViewRef;
-    let { className, style, onFirstAppear, onAppear, ...rest } = props;
-
-    let handleAppear = onAppear;
-    if (onFirstAppear) {
-      handleAppear = (event) => {
-        onAppear && onAppear(event);
-        if (!selfRef.triggeredAppear) {
-          onFirstAppear && onFirstAppear(event);
-        } else {
-          selfRef.triggeredAppear = true;
-        }
-      };
-    }
-    return <div {...rest} onAppear={handleAppear} ref={ref} className={cx( isWeex ? '' : 'rax-view-v2', className)} style={style} />;
-  }
-);
-
-export default wrapDefaultProperties(View);
+export default View;

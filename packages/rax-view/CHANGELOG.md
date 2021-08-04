1ec24727e0eaea57a2b03120ae3d5e72a07de0a8
fushen <fushen.jzw@alibaba-inc.com>
2021-08-04 14:22:05 +0800

    fix: onFirstAppear triggered more than once

--- packages/rax-view/CHANGELOG.md
+++ packages/rax-view/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 2.1.1
+
+- [fix] `onFirstAppear` triggered more than once
+
 ## 2.1.0
 
 - [feat] Support exports field
--- packages/rax-view/src/index.tsx
+++ packages/rax-view/src/index.tsx
@@ -1,17 +1,32 @@
-import { isMiniApp, isWeb } from 'universal-env';
-import ViewCommon from './common';
-import ViewMiniApp from './miniapp';
+import { createElement, useRef, forwardRef, ForwardRefExoticComponent, MutableRefObject } from 'rax';
+import cx from 'classnames/dedupe';
+import { isWeex } from 'universal-env';
+import wrapDefaultProperties from './utils/wrapDefaultProperties';
+import { ViewProps } from './types';
+import './index.css';
 
-let View = null;
-
-if (isWeb) {
-  View = ViewCommon;
-} else if (isMiniApp) {
-  View = ViewMiniApp;
-} else {
-  View = ViewCommon;
+interface ViewRef extends MutableRefObject<HTMLDivElement> {
+  triggeredAppear: boolean;
 }
 
-export default View;
+const View: ForwardRefExoticComponent<ViewProps> = forwardRef(
+  (props, ref) => {
+    const selfRef: ViewRef = useRef(null) as ViewRef;
+    let { className, style, onFirstAppear, onAppear, ...rest } = props;
 
+    let handleAppear = onAppear;
+    if (onFirstAppear) {
+      handleAppear = (event) => {
+        onAppear && onAppear(event);
+        if (!selfRef.triggeredAppear) {
+          onFirstAppear && onFirstAppear(event);
+        } else {
+          selfRef.triggeredAppear = true;
+        }
+      };
+    }
+    return <div {...rest} onAppear={handleAppear} ref={ref} className={cx( isWeex ? '' : 'rax-view-v2', className)} style={style} />;
+  }
+);
 
+export default wrapDefaultProperties(View);

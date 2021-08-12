c8e362931288cb5bbdfa5bac01678ce8d8f9e59c
炼玉 <lianyu.ysj@alibaba-inc.com>
2021-08-12 16:57:01 +0800

    fix: 修复兼容问题

--- packages/rax-scrollview/src/weex/index.tsx
+++ packages/rax-scrollview/src/weex/index.tsx
@@ -21,7 +21,63 @@ const ScrollView: ForwardRefExoticComponent<ScrollViewProps> = forwardRef(
   (props, ref) => {
     /* global __weex_v2__ */
     if (typeof __weex_v2__ === 'object') {
-      return (<scroller {...props} > {props.children} </scroller>);
+      let {
+        className,
+        horizontal,
+        showsHorizontalScrollIndicator,
+        showsVerticalScrollIndicator,
+        onEndReached,
+        onEndReachedThreshold,
+        onScroll,
+        children
+      } = props;
+      const handleScroll = e => {
+        e.nativeEvent = {
+          contentOffset: {
+            // HACK: weex scroll event value is opposite of web
+            x: -e.contentOffset.x,
+            y: -e.contentOffset.y
+          },
+          contentSize: e.contentSize
+            ? {
+              width: e.contentSize.width,
+              height: e.contentSize.height
+            }
+            : null
+        };
+        onScroll(e);
+      };
+
+      // In weex must be int value
+      onEndReachedThreshold =
+        typeof onEndReachedThreshold === 'string'
+          ? parseInt(onEndReachedThreshold, 10)
+          : onEndReachedThreshold;
+
+      const cls = cx(
+        baseCls,
+        `${baseCls}-${horizontal ? 'horizontal' : 'vertical'}`,
+        className
+      );
+      let showsScrollIndicator = horizontal
+        ? showsHorizontalScrollIndicator
+        : showsVerticalScrollIndicator;
+
+      const weexProps = { ...props };
+      delete weexProps.onEndReachedThreshold;
+      return (
+        <scroller
+          {...weexProps}
+          className={cls}
+          showScrollbar={showsScrollIndicator}
+          onLoadmore={onEndReached}
+          onScroll={onScroll ? handleScroll : null}
+          loadmoreoffset={onEndReachedThreshold}
+          scrollDirection={horizontal ? 'horizontal' : 'vertical'}
+        >
+          {children}
+        </scroller>
+      );
     }
     let {
       className,

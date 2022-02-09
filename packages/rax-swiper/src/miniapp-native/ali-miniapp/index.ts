e4a8dc01af8b13df8b610fa3be4c9c22f6d4cc2f
chenrongyan.cry <chenrongyan.cry@alibaba-inc.com>
2022-02-09 11:55:43 +0800

    fix: reset miniapp

--- packages/rax-swiper/src/miniapp-native/ali-miniapp/index.ts
+++ packages/rax-swiper/src/miniapp-native/ali-miniapp/index.ts
@@ -1,103 +1,79 @@
 import fmtEvent from './fmtEvent';
 
+const noop = () => {};
 Component({
   data: {
     current: 0,
-    children: []
+    duration: 0,
   },
-  properties: {
-    className: {
-      type: String,
-    },
-    styleSheet: {
-      type: String,
-      value: ''
-    },
-    autoplay: {
-      type: Boolean,
-      value: false,
-    },
-    pagination: {
-      type: Boolean,
-      value: true,
-    },
-    loop: {
-      type: Boolean,
-      value: true,
-    },
-    direction: {
-      type: String,
-      value: 'horizontal',
-    },
-    initialSlide: {
-      type: Number,
-      value: 0,
-    },
+  props: {
+    className: '',
+    styleSheet: '',
+    autoplay: false,
+    pagination: true,
+    loop: true,
+    initialSlide: 0,
+    interval: 3000,
+    direction: 'horizontal',
+    onChange: noop,
     paginationStyle: {
-      type: Object,
-      value: {
-        itemColor: 'rgba(0, 0, 0, .3)',
-        itemActiveColor: '#000000',
-      },
+      itemColor: 'rgba(0, 0, 0, .3)',
+      itemActiveColor: '#000000',
     },
-    interval: {
-      type: Number,
-      value: 3000
-    },
-    __length: {
-      type: Number,
-      observer(newLength) {
-        if (this.data.children.length !== newLength) {
-          this.data.children.length = newLength;
-          this.setData({
-            children: this.data.children
-          });
-        }
-      }
-    }
+    children: [],
+    __length: 0
   },
-  options: {
-    multipleSlots: true,
-    styleIsolation: 'apply-shared'
-  },
-  attached() {
+  onInit() {
     this.setData({
-      current: this.properties.initialSlide,
+      current: this.props.initialSlide,
+      duration: 500
     });
   },
-  observers: {
-    'initialSlide': function(nextIndex) {
-      if (this.properties.initialSlide !== nextIndex && this.data.current !== nextIndex) {
+  didMount() {
+    if (!my.canIUse('component2')) {
+      /**
+       * The default duration is 500.
+       * There should be no transition animation when the current is initialized,
+       * so the initial duration is 0.
+       * After initialization, the value is set to the default value.
+       */
+      this.setData({
+        current: this.props.initialSlide,
+      }, () => {
         this.setData({
-          current: nextIndex
+          duration: 500
         });
-      }
+      });
+    }
+  },
+  didUpdate(prevProps) {
+    if (prevProps.initialSlide !== this.props.initialSlide && this.props.initialSlide !== this.data.current) {
+      this.setData({
+        current: this.props.initialSlide
+      });
     }
   },
   methods: {
     onChange(e) {
-      const event = fmtEvent(this.properties, e);
-      this.triggerEvent('onSlideChange', {
+      const event = fmtEvent(this.props, e);
+      this.props.onChange({
         activeIndex: event.detail.current,
         activeItemId: event.detail.currentItemId
       });
-      // Only setData by user touch action
-      if (e.detail.source === 'touch') {
-        this.setData({
-          current: event.detail.current
-        });
-      }
+      this.setData({
+        current: event.detail.current
+      });
     },
     slideTo(index) {
-      if (typeof index === 'number' && index < this.data.__length && index >= 0) {
+      if (typeof index === 'number' && index < this.props.__length && index >= 0) {
         this.setData({
           current: index,
         });
       }
     },
     slideNext() {
-      const current = this.data.current + 1;
-      if (current >= this.data.__length) {
+      let current = this.data.current + 1;
+      if (current >= this.props.__length) {
         return;
       }
       this.setData({
@@ -105,7 +81,7 @@ Component({
       });
     },
     slidePrev() {
-      const current = this.data.current - 1;
+      let current = this.data.current - 1;
       if (current < 0) {
         return;
       }
